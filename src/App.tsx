a005cefb0138b10517d698d0bc5d0edcfedc2dac
Zohaib-Ramzan <zohaibramzan711@gmail.com>
2023-07-27 00:44:23 +0500

    Setup the services

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {View , Text , StyleSheet , SafeAreaView} from "react-native"
+
+const App = (): JSX.Element => {
+  return(
+    <SafeAreaView>
+      <Text>
+        HelloWorlds!!!
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container : {
+    
+  }
+})
+
+export default App
